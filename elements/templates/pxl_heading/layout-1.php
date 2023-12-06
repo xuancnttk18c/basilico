b1f3279682d38294d6e320fa99243717db8ce843
Tony <153053938+tony7iquid@users.noreply.github.com>
2023-12-06 16:26:20 +0700

    Update layout-1.php

--- elements/templates/pxl_heading/layout-1.php
+++ elements/templates/pxl_heading/layout-1.php
@@ -81,7 +81,7 @@ extract($settings);
 
 ?>
 <div <?php pxl_print_html($widget->get_render_attribute_string( 'wrap-heading' )); ?>>
-    <div class="pxl-heading-inner <?php echo esc_attr($text_align); ?>">
+    <div class="pxl-heading-inner abc <?php echo esc_attr($text_align); ?>">
         <?php if(!empty($sub_title)) : ?>
             <div <?php pxl_print_html($widget->get_render_attribute_string( 'sub-title' )); ?>>
                 <span><?php pxl_print_html(nl2br($sub_title)); ?></span>
