1ac2d16f271d48f46a0358049f78eb9ac0159a85
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-07 14:57:55 +0700

    Update layout-2.php

--- elements/templates/pxl_cart/layout-2.php
+++ elements/templates/pxl_cart/layout-2.php
@@ -12,39 +12,39 @@ $cart_text = $widget->get_setting('cart_text', 'Your Cart');
             $count = WC()->cart->get_cart_contents_count();
         if( $widget->get_setting('icon_type','none') == 'lib'){
             echo '<div class="pxl-cart-icon d-inline-flex">';
-                \Elementor\Icons_Manager::render_icon($settings['selected_icon'], ['aria-hidden' => 'true', 'class' => 'pxli'], 'i');
-                if(!is_admin()): ?>
-                    <span class="pxl-cart-text"><?php echo esc_html($cart_text); ?></span>
-                    <span class="pxl-cart-count cart_total"><?php echo esc_attr($count) ?></span>
-                <?php endif;
+            \Elementor\Icons_Manager::render_icon($settings['selected_icon'], ['aria-hidden' => 'true', 'class' => 'pxli'], 'i');
+            if(!is_admin()): ?>
+                <span class="pxl-cart-text"><?php echo esc_html($cart_text); ?></span>
+                <span class="pxl-cart-count cart_total"><?php echo esc_attr($count) ?></span>
+            <?php endif;
             echo '</div>';
         }
         else if ( $widget->get_setting('icon_type','none') == 'custom') {
             echo '<div class="pxl-cart-icon d-inline-flex align-items-center">';
-                echo
-                '<div class="custom-cart-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+            ?>
+            <div class="custom-cart-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
                     <style type="text/css">
-                    .st0{fill:#CA2F06;}
-                    .st1{fill:#161A1B;}
+                        .st0{fill:#CA2F06;}
+                        .st1{fill:#161A1B;}
                     </style>
                     <path class="st0" d="M508.3,85.8c-3-3.6-7.6-5.8-12.3-5.8H107.5c-5,0-9.7,2.3-12.7,6.2c-3,4-4,9.1-2.8,13.9l51.2,191.4  c1.9,7,8.2,11.9,15.5,11.9c0.3,0,0.5,0,0.8,0L464.8,288c7.4-0.4,13.6-5.8,14.9-13.1l32-176C512.6,94.2,511.3,89.4,508.3,85.8z   M224,224c0,8.8-7.2,16-16,16s-16-7.2-16-16v-64c0-8.8,7.2-16,16-16s16,7.2,16,16V224z M320,224c0,8.8-7.2,16-16,16s-16-7.2-16-16  v-64c0-8.8,7.2-16,16-16s16,7.2,16,16V224z M416,224c0,8.8-7.2,16-16,16s-16-7.2-16-16v-64c0-8.8,7.2-16,16-16s16,7.2,16,16V224z"/>
                     <g>
-                    <circle class="st1" cx="208" cy="448" r="48"/>
-                    <circle class="st1" cx="400" cy="448" r="48"/>
-                    <path class="st1" d="M432,368H176c-7.3,0-13.6-4.9-15.5-11.9L78.3,48H16C7.2,48,0,40.8,0,32s7.2-16,16-16h74.6   c7.3,0,13.6,4.9,15.5,11.9L188.3,336H432c8.8,0,16,7.2,16,16S440.8,368,432,368z"/>
+                        <circle class="st1" cx="208" cy="448" r="48"/>
+                        <circle class="st1" cx="400" cy="448" r="48"/>
+                        <path class="st1" d="M432,368H176c-7.3,0-13.6-4.9-15.5-11.9L78.3,48H16C7.2,48,0,40.8,0,32s7.2-16,16-16h74.6   c7.3,0,13.6,4.9,15.5,11.9L188.3,336H432c8.8,0,16,7.2,16,16S440.8,368,432,368z"/>
                     </g>
-                    </svg>
-                </div>';
-                if(!is_admin()): ?>
-                    <div class="pxl-cart-text">
-                        <?php echo esc_html($cart_text); ?>
-                        <span class="pxl-cart-count cart_total"><?php echo esc_attr($count) ?></span>
-                    </div>
-                <?php endif;
+                </svg>
+            </div>
+            <?php
+            if(!is_admin()): ?>
+                <div class="pxl-cart-text">
+                    <?php echo esc_html($cart_text); ?>
+                    <span class="pxl-cart-count cart_total"><?php echo esc_attr($count) ?></span>
+                </div>
+            <?php endif;
             echo '</div>';
         }
         ?>
     </div>
 </div>
- 
