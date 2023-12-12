23664d830c02332d985fe5068a23c82bcb1e8368
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-12 16:13:03 +0700

    Update class-blog.php

--- inc/classes/class-blog.php
+++ inc/classes/class-blog.php
@@ -12,7 +12,7 @@ if (!class_exists('Basilico_Blog')) {
             } else {
                 $thumbnail_size = 'size-post-single';
             }
-            var_dump(basilico_configs('custom_sizes'));
+            var_dump(basilico_configs('custom_sizes')['size-post-single']);
             the_post_thumbnail($thumbnail_size);
         }
 
