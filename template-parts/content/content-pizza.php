f124a906120814a1e1e0fa75d79c9ff9e7ef6606
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-13 15:15:00 +0700

    Update content-pizza.php

--- template-parts/content/content-pizza.php
+++ template-parts/content/content-pizza.php
@@ -10,7 +10,7 @@ $post_social_share = basilico()->get_theme_opt('post_social_share', false);
 $featured_video = get_post_meta(get_the_ID(), 'featured-video-url', true);
 $audio_url = get_post_meta(get_the_ID(), 'featured-audio-url', true);
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class('pxl-archive-post row'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('pxl-archive-post row gx-0'); ?>>
     <?php if (has_post_thumbnail()) { ?>
         <div class="post-featured col-5">
             <div class="post-image">
