fda78f63a4caff95df86db187df97bb4c2f86993
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-19 09:46:33 +0700

    add pxl-tabs layout-5

--- elements/register/pxl_tabs.php
+++ elements/register/pxl_tabs.php
@@ -38,6 +38,10 @@ pxl_add_custom_widget(
                                     'label' => esc_html__( 'Layout 4', 'basilico' ),
                                     'image' => get_template_directory_uri() . '/elements/assets/layout-image/pxl_tabs-1.jpg'
                                 ],
+                                '5' => [
+                                    'label' => esc_html__( 'Layout 5', 'basilico' ),
+                                    'image' => get_template_directory_uri() . '/elements/assets/layout-image/pxl_tabs-1.jpg'
+                                ],
                             ],
                             'prefix_class' => 'pxl-tabs-layout-',
                         ),
