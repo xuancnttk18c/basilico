625b7877af36694644468acca467850c5b015203
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-22 15:34:05 +0700

    Update layout-6.php

--- elements/templates/pxl_tabs/layout-6.php
+++ elements/templates/pxl_tabs/layout-6.php
@@ -24,12 +24,12 @@ if(count($tabs_list) > 0){
                         $widget->add_render_attribute( $title_key, 'class', 'active');
                     }
                     ?>
-                    <div class="title-icon">
-                        <?php if(! empty( $settings['selected_icon']['value'] )): ?>
-                            <?php \Elementor\Icons_Manager::render_icon( $settings['selected_icon'], [ 'aria-hidden' => 'true', 'class' => 'pxl-fancy-icon pxl-icon' ], 'i' );?>
-                        <?php endif; ?>
-                    </div> 
                     <span <?php pxl_print_html($widget->get_render_attribute_string( $title_key )); ?> data-slide="<?php echo esc_attr($key); ?>">
+                        <div class="title-icon">
+                            <?php if(! empty( $settings['selected_icon']['value'] )): ?>
+                                <?php \Elementor\Icons_Manager::render_icon( $settings['selected_icon'], [ 'aria-hidden' => 'true', 'class' => 'pxl-fancy-icon pxl-icon' ], 'i' );?>
+                            <?php endif; ?>
+                        </div>
                         <span><?php echo pxl_print_html($tab['tab_title']); ?></span>
                     </span>
                 <?php endforeach; ?>
