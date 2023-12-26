b9d9992374a68e4a0e29c7ce1c27459434ef33ae
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-26 14:02:18 +0700

    Update layout-1.php

--- elements/templates/pxl_testimonial_carousel/layout-1.php
+++ elements/templates/pxl_testimonial_carousel/layout-1.php
@@ -62,7 +62,9 @@ $widget->add_render_attribute('carousel', [
                         <div class="pxl-swiper-slide swiper-slide">
                             <div class="item-inner relative text-center">
                                 <?php if ($quote_icon_type == 'icon' && !empty($settings['selected_icon']['value'])) : ?>
-                                    <?php \Elementor\Icons_Manager::render_icon($settings['selected_icon'], ['aria-hidden' => 'true', 'class' => 'item-quote-icon pxl-icon'], 'i'); ?>
+                                    <div class="icon-wrapper">
+                                        <?php \Elementor\Icons_Manager::render_icon($settings['selected_icon'], ['aria-hidden' => 'true', 'class' => 'item-quote-icon pxl-icon'], 'i'); ?>
+                                    </div>
                                 <?php else : ?>
                                     <div class="item-quote-icon">“</div>
                                 <?php endif; ?>
