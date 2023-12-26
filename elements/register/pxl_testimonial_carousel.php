116d56f0061a7533812cc3b61ca7308581fe3f5f
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-26 11:47:25 +0700

    Update pxl_testimonial_carousel.php

--- elements/register/pxl_testimonial_carousel.php
+++ elements/register/pxl_testimonial_carousel.php
@@ -121,15 +121,6 @@ pxl_add_custom_widget(
                             'name'        => 'button_link',
                             'label'       => esc_html__( 'Button Link', 'basilico' ),
                             'type'        => \Elementor\Controls_Manager::URL,
-                            'placeholder' => esc_html__( 'https://your-link.com', 'basilico' ),
-                            'default'     => [
-                                'url'         => '#',
-                                'is_external' => 'on'
-                            ],
-                            'condition' => [
-                                'layout' => '6',
-                                'show_button' => 'true'
-                            ],
                         ),
                     ),
                 ),
