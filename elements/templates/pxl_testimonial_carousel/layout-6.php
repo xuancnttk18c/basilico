e7b1ca38243bf3e84b65ad322f2df91efcefa1b2
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-26 10:00:27 +0700

    Update layout-6.php

--- elements/templates/pxl_testimonial_carousel/layout-6.php
+++ elements/templates/pxl_testimonial_carousel/layout-6.php
@@ -42,7 +42,7 @@ $widget->add_render_attribute( 'carousel', [
 <?php if(isset($content_list) && !empty($content_list) && count($content_list)): ?>
     <div class="pxl-swiper-slider pxl-testimonial-carousel layout-<?php echo esc_attr($settings['layout'])?>">
         <div class="pxl-swiper-thumbs-wrap d-flex justify-content-center">
-            <div class="pxl-swiper-thumbs overflow-hidden" data-drag = "false" data-center = "true" data-item ="5" data-gutter ="20">
+            <div class="pxl-swiper-thumbs overflow-hidden" data-item ="5" data-gutter ="20">
                 <div class="pxl-thumbs-wrapper swiper-wrapper ">
                     <?php
                     $idx = 0;
