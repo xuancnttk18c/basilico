4a2bb40006ed13bb9265e6d65918e864d2907d11
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2023-12-27 10:19:45 +0700

    Update layout-3.php

--- elements/templates/pxl_testimonial_carousel/layout-3.php
+++ elements/templates/pxl_testimonial_carousel/layout-3.php
@@ -66,9 +66,8 @@ $widget->add_render_attribute('carousel', [
                                     <div class="icon-wrapper">
                                         <?php \Elementor\Icons_Manager::render_icon($settings['selected_icon'], ['aria-hidden' => 'true', 'class' => 'item-quote-icon pxl-icon'], 'i'); ?>
                                     </div>
-                                <?php if ($quote_icon_type == 'text') : ?>
+                                <?php elseif ($quote_icon_type == 'text') : ?>
                                     <div class="item-quote-icon">“</div>
-                                <?php endif; ?>
                                 <?php else : ?>
                                 <?php endif; ?>
                                 <?php if (!empty($testimonial_title)) { ?>
