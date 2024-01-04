17d0942288f74aa59b7c3313807901b309a53481
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-01-04 17:15:15 +0700

    Update pxl-pagination.php

--- woocommerce/loop/pxl-pagination.php
+++ woocommerce/loop/pxl-pagination.php
@@ -8,11 +8,13 @@ $current = isset( $current ) ? $current : wc_get_loop_prop( 'current_page' );
 $base    = isset( $base ) ? $base : esc_url_raw( str_replace( 999999999, '%#%', remove_query_arg( 'add-to-cart', get_pagenum_link( 999999999, false ) ) ) );
 $format  = isset( $format ) ? $format : '';
 
+$style = basilico()->get_theme_opt('archive_pagination_style', 'style-df');
+
 if ( $total <= 1 ) {
 	return;
 }
 ?>
-<nav class="woocommerce-pagination abc">
+<nav class="woocommerce-pagination <?php echo esc_attr($style); ?>">
 	<?php
 	echo paginate_links(
 		apply_filters(
