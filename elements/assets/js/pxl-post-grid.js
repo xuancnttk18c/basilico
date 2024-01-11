bd574d8f257ba3760542272e17e4bbd97c001482
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-01-11 14:40:16 +0700

    Update pxl-post-grid.js

--- elements/assets/js/pxl-post-grid.js
+++ elements/assets/js/pxl-post-grid.js
@@ -217,6 +217,7 @@
             $(this).hover(function() {
                 $(this).find('.item-excerpt').css('max-height', excerptHeight + 'px');
                 $(this).find('.post-image').css('max-height', (imageHeight - excerptHeight) + 'px');
+                console.log($(this).find('.post-image').outerHeight());
             }, function() {
                 $(this).find('.item-excerpt').css('max-height', '0px');
                 $(this).find('.post-image').css('max-height', imageHeight + 'px');
