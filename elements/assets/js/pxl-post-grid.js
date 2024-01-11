7fcc44db7a87450ac3b623d0ed0c953cecccaf33
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-01-11 15:23:39 +0700

    update code

--- elements/assets/js/pxl-post-grid.js
+++ elements/assets/js/pxl-post-grid.js
@@ -212,7 +212,8 @@
         $('.pxl-post-grid.layout-post-2 .grid-item').each(function() {
             var excerptHeight = $(this).find('.item-excerpt').get(0).scrollHeight;
             var imageHeight = $(this).find('.post-image').outerHeight();
-            //$(this).find('.item-excerpt').css('max-height', '0px');
+            $(this).find('.item-excerpt').css('max-height', '0px');
+            $(this).find('.post-image').css('max-height', imageHeight + 'px');
 
             $(this).hover(function() {
                 $(this).find('.item-excerpt').css('max-height', excerptHeight + 'px');
