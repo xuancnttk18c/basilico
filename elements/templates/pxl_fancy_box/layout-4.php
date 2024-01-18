dbc68952ef7e513f935188c7e7789a069b6df088
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-01-18 16:08:09 +0700

    Update layout-4.php

--- elements/templates/pxl_fancy_box/layout-4.php
+++ elements/templates/pxl_fancy_box/layout-4.php
@@ -1,33 +1,7 @@
 <?php
 use Elementor\Utils;
-if(!empty($settings['link']['url'])){
-    $widget->add_render_attribute( 'link', 'href', $settings['link']['url'] );
-    $widget->add_render_attribute( 'link', 'class', 'btn '.$settings['style'] );
-    if ( $settings['link']['is_external'] ) {
-        $widget->add_render_attribute( 'link', 'target', '_blank' );
-    }
-    if ( $settings['link']['nofollow'] ) {
-        $widget->add_render_attribute( 'link', 'rel', 'nofollow' );
-    }
-    if ( ! empty( $settings['link']['custom_attributes'] ) ) {
-        // Custom URL attributes should come as a string of comma-delimited key|value pairs
-        $custom_attributes = Utils::parse_custom_attributes( $settings['link']['custom_attributes'] );
-        $widget->add_render_attribute( 'link', $custom_attributes);
-    }
-
-}
-$link_attributes = $widget->get_render_attribute_string( 'link' );
-
-$thumbnail = '';
-if( ! empty( $settings['selected_img']['id'] ) ){
-    $img  = pxl_get_image_by_size( array(
-        'attach_id'  => $settings['selected_img']['id'],
-        'thumb_size' => '370x370',
-    ) );
-    $thumbnail = $img['thumbnail'];
-}
 ?>
- 
+
 <div class="pxl-fancy-box layout-4">
     <div class="box-inner">
         <div class="box-icon">
@@ -40,17 +14,11 @@ if( ! empty( $settings['selected_img']['id'] ) ){
                 <h3 class="box-title">
                     <?php pxl_print_html( nl2br($widget->get_setting('title'))); ?>
                 </h3>
+                <div class="pxl-divider"></div>
             <?php endif; ?>
             <?php if(!empty($widget->get_setting('description'))): ?>
                 <div class="box-description">
                     <?php pxl_print_html($widget->get_setting('description')); ?>
-                </div>
-            <?php endif; ?>
-            <?php if (!empty($widget->get_setting('button_text'))) : ?>
-                <div class="box-button">
-                    <?php if ( $link_attributes ) echo '<a '. implode( ' ', [ $link_attributes ] ).'>'; ?>
-                    <span><?php pxl_print_html( nl2br($widget->get_setting('button_text'))); ?></span>
-                    <?php if ( $link_attributes ) echo '</a>'; ?> 
                 </div>
             <?php endif; ?>
         </div>
