8be8146fd7152d41a8c0374ccfbf2b8451e838e7
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-03-06 10:43:57 +0700

    Update theme-config.php

--- inc/theme-config.php
+++ inc/theme-config.php
@@ -148,7 +148,7 @@ if(!function_exists('basilico_configs')){
                 'font-weight'       => basilico()->get_theme_opt('font_input', ['font-weight' => '400'], 'font-weight'),
                 'line-height'       => basilico()->get_theme_opt('font_input', ['line-height' => '1.66666666667'], 'line-height'),
                 'letter-spacing'    => basilico()->get_theme_opt('font_input', ['letter-spacing' => '0.01em'], 'letter-spacing'),
-                'padding'           => basilico()->get_theme_opt('input_padding', ['left' => '20px', 'units' => 'px'])['left'],
+                'padding'           => basilico()->get_theme_opt('input_padding', ['padding-left' => '20px', 'units' => 'px'])['padding-left'],
             ],
         ];
 
