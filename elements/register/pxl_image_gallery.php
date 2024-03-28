7d0f37c9fa1f3e7395fc327ab87b4e9e172428ee
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-03-28 10:49:11 +0700

    Update pxl_image_gallery.php

--- elements/register/pxl_image_gallery.php
+++ elements/register/pxl_image_gallery.php
@@ -49,7 +49,7 @@ pxl_add_custom_widget(
                                 ],
                                 '7' => [
                                     'label' => esc_html__( 'Layout 7', 'basilico' ),
-                                    'image' => get_template_directory_uri() . '/elements/assets/layout-image/pxl_image_gallery-6.jpg'
+                                    'image' => get_template_directory_uri() . '/elements/assets/layout-image/pxl_image_gallery-7.jpg'
                                 ],
                             ],
                             'prefix_class' => 'pxl-image-gallery-layout-',
