c619dffe290ab6b76ea39e1511262574f8911ff1
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-04-19 11:07:18 +0700

    update

--- elements/register/pxl_fancy_box.php
+++ elements/register/pxl_fancy_box.php
@@ -39,6 +39,10 @@ pxl_add_custom_widget(
                                     'label' => esc_html__( 'Layout 5', 'basilico' ),
                                     'image' => get_template_directory_uri() . '/elements/assets/layout-image/pxl_fancy_box-1.jpg'
                                 ],
+                                '6' => [
+                                    'label' => esc_html__( 'Layout 6', 'basilico' ),
+                                    'image' => get_template_directory_uri() . '/elements/assets/layout-image/pxl_fancy_box-1.jpg'
+                                ],
                             ],
                         )
                     )
