fdfc03787b02f74e02fd91464b6cb254a6077771
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-07 09:50:26 +0700

    Update wc-functions.php

--- pxl-nutrition/wc-functions.php
+++ pxl-nutrition/wc-functions.php
@@ -4,7 +4,6 @@ function add_ppwp_access_link_tab($tabs) {
     $tabs['ppwp_woo'] = array(
         'label' => 'PPWP Access Link',
         'target' => 'ppwp_woo_options',
-        'class' => array( 'show_if_virtual' ),
         'priority' => 65,
     );
     return $tabs;
