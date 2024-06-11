4c544fc121f237e9cbc36cb5ee0381af12eadcca
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-11 09:27:52 +0700

    update

--- elements/templates/pxl_product_grid/layout-1.php
+++ elements/templates/pxl_product_grid/layout-1.php
@@ -25,49 +25,52 @@ if( $total <= 0){
     echo '<div class="pxl-no-post-grid">'.esc_html__( 'No Post Found', 'basilico' ). '</div>';
     return;
 }
+
+$product_layout = basilico()->get_theme_opt('product_layout', 'layout-1');
 ?>
 
-<div id="<?php echo esc_attr($wg_id) ?>" class="pxl-product-grid <?php echo esc_attr($grid_layout) ?>" data-layout=".<?php echo esc_attr($grid_layout) ?>">
-    <div <?php pxl_print_html($widget->get_render_attribute_string('grid')); ?>>
-        <?php
-        while ($query->have_posts()) {
-            $query->the_post();
-            wc_get_template( 'pxl-content-product-grid-1.php', $settings );
-        }
-        ?>
-        <?php wp_reset_postdata(); ?>
-    </div>
-
-    <?php if ($pagination_type == 'pagination' || $pagination_type == 'loadmore' || $pagination_type == 'infinite' ) { ?>
-        <div class="pxl-product-grid-pagination grid-df-pagin d-flex justify-content-center">
+<div class="<?php echo 'pxl-shop-'.esc_attr($product_layout); ?>">
+    <div id="<?php echo esc_attr($wg_id) ?>" class="pxl-product-grid <?php echo esc_attr($grid_layout) ?>" data-layout=".<?php echo esc_attr($grid_layout) ?>">
+        <div <?php pxl_print_html($widget->get_render_attribute_string('grid')); ?>>
             <?php
-            if ( empty( $query ) ){
-                $query = $GLOBALS['wp_query'];
-            }
-            if ( !empty( $query->max_num_pages ) && is_numeric( $query->max_num_pages ) && $query->max_num_pages >= 2 ){
-                $paged = $query->get( 'paged', '' );
-
-                if ( ! $paged && is_front_page() && ! is_home() )
-                {
-                    $paged = $query->get( 'page', '' );
-                }
-
-                $paged = $paged ? intval( $paged ) : 1;
-                $pagin_args = array(
-                    'total'           => $query->max_num_pages,
-                    'current'         => $paged,
-                    'base'            => esc_url_raw( add_query_arg( 'product-page', '%#%', false ) ),
-                    'format'          => '?product-page=%#%',
-                    'pagination_type' => $pagination_type,
-                    'limit'           => $post_per_page,
-                    'loadmore_text'   => $settings['loadmore_text'],
-                    'class'           => 'el-widget',
-                    'total_posts'     => $total
-                );
-                wc_get_template( 'loop/pagination-custom.php', $pagin_args );
+            while ($query->have_posts()) {
+                $query->the_post();
+                wc_get_template_part( 'pxl-content-product', esc_attr($product_layout) );
             }
             ?>
+            <?php wp_reset_postdata(); ?>
         </div>
-    <?php } ?>
 
+        <?php if ($pagination_type == 'pagination' || $pagination_type == 'loadmore' || $pagination_type == 'infinite' ) { ?>
+            <div class="pxl-product-grid-pagination grid-df-pagin d-flex justify-content-center">
+                <?php
+                if ( empty( $query ) ){
+                    $query = $GLOBALS['wp_query'];
+                }
+                if ( !empty( $query->max_num_pages ) && is_numeric( $query->max_num_pages ) && $query->max_num_pages >= 2 ){
+                    $paged = $query->get( 'paged', '' );
+
+                    if ( ! $paged && is_front_page() && ! is_home() )
+                    {
+                        $paged = $query->get( 'page', '' );
+                    }
+
+                    $paged = $paged ? intval( $paged ) : 1;
+                    $pagin_args = array(
+                        'total'           => $query->max_num_pages,
+                        'current'         => $paged,
+                        'base'            => esc_url_raw( add_query_arg( 'product-page', '%#%', false ) ),
+                        'format'          => '?product-page=%#%',
+                        'pagination_type' => $pagination_type,
+                        'limit'           => $post_per_page,
+                        'loadmore_text'   => $settings['loadmore_text'],
+                        'class'           => 'el-widget',
+                        'total_posts'     => $total
+                    );
+                    wc_get_template( 'loop/pagination-custom.php', $pagin_args );
+                }
+                ?>
+            </div>
+        <?php } ?>
+    </div>
 </div>
--- pxl-nutrition/wc-functions.php
+++ pxl-nutrition/wc-functions.php
@@ -90,7 +90,7 @@ function pxl_nutrition_frontend_display() {
 function get_nutrition_opts() {
     $opts = array(
         'pxl_nutrition_calories'    => array(
-            'label'                 => esc_html__( 'Calories: ', 'basilico' ),
+            'label'                 => esc_html_( 'Calories: ', 'basilico' ),
             'placeholder'           => esc_html('550kcal', 'basilico'),
         ),
         'pxl_nutrition_carbohydrates' => array(
