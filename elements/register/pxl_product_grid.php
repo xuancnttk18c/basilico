21651f8924cb00fb0db2ec61062ce6b45a37f923
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-11 09:39:04 +0700

    update

--- elements/register/pxl_product_grid.php
+++ elements/register/pxl_product_grid.php
@@ -37,6 +37,18 @@ pxl_add_custom_widget(
                     'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
                     'controls' => array(
                         array(
+                            'id'       => 'product_layout',
+                            'type'     => 'select',
+                            'title'    => esc_html__('Product Layout', 'basilico'),
+                            'options'  => [
+                                'layout-1'       => esc_html__('Layout 1', 'basilico'),
+                                'layout-2'       => esc_html__('Layout 2', 'basilico'),
+                                'layout-3'       => esc_html__('Layout 3', 'basilico'),
+                                'layout-5'       => esc_html__('Layout 5', 'basilico'),
+                            ],
+                            'default'  => 'layout-1',
+                        ),
+                        array(
                             'name'    => 'query_type',
                             'label'   => esc_html__( 'Select Query Type', 'basilico' ),
                             'type'    => 'select',
@@ -139,7 +151,7 @@ pxl_add_custom_widget(
                     ),
                 ),
             ),
-        ),
-    ),
-    basilico_get_class_widget_path()
+),
+),
+basilico_get_class_widget_path()
 );
