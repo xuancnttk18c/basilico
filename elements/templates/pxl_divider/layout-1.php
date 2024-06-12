353a7a5176395ecad355adf5473d0cabf7f6e911
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-12 10:47:17 +0700

    Update layout-1.php

--- elements/templates/pxl_divider/layout-1.php
+++ elements/templates/pxl_divider/layout-1.php
@@ -1,11 +1,10 @@
 <?php
 	$style = $widget->get_setting('style', 'style-1');
+	$alignment = $widget->get_setting('alignment', 'left');
 	$draw = $widget->get_setting('draw', '');
-	extract($settings);
-	var_dump($settings);
 ?>
 
-<div class="pxl-widget-divider <?php echo esc_attr($settings['alignment']); ?>">
+<div class="pxl-widget-divider <?php echo esc_attr($alignment); ?>">
 	<div class="pxl-divider <?php echo esc_attr($style); ?><?php echo esc_attr($draw) == 'true' ? ' pxl-scroll' : ''; ?>">
 		<?php if ($style == 'style-2') : ?>
 			<div class="diamond-icon"></div>
