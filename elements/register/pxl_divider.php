875ac9c7d88daf01dc21ceed437b4fa161819c1c
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-16 09:36:51 +0700

    update

--- elements/register/pxl_divider.php
+++ elements/register/pxl_divider.php
@@ -80,7 +80,7 @@ pxl_add_custom_widget(
                             'label' => esc_html__( 'Alignment', 'basilico' ),
                             'type' => \Elementor\Controls_Manager::CHOOSE,
                             'control_type' => 'responsive',
-                            'prefix_class' => 'divider-align-%s',
+                            'prefix_class' => 'pxl-divider-align-%s',
                             'options' => [
                                 'left' => [
                                     'title' => esc_html__( 'Left', 'basilico' ),
