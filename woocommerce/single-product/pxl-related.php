7733b774c7b3e6e55883f52683a3dcc034a316fb
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-22 08:51:08 +0700

    Update pxl-related.php

--- woocommerce/single-product/pxl-related.php
+++ woocommerce/single-product/pxl-related.php
@@ -86,6 +86,10 @@ if ( $related_products ) : ?>
                         </div>
                     </div>
                 </div>
+                <div class="pxl-swiper-arrows single-loop-swiper-arrows">
+                	<div class="pxl-swiper-arrow pxl-swiper-arrow-prev"><span class="pxl-icon lnil lnil-arrow-left"></span></div>
+                	<div class="pxl-swiper-arrow pxl-swiper-arrow-next"><span class="pxl-icon lnil lnil-arrow-right"></span></div>
+                </div>
             </div>
         </div>
     </section>
