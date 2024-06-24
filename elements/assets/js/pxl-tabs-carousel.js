46280ed5420da27a587e01c333ffec59ca140532
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-24 15:48:42 +0700

    Update pxl-tabs-carousel.js

--- elements/assets/js/pxl-tabs-carousel.js
+++ elements/assets/js/pxl-tabs-carousel.js
@@ -18,7 +18,10 @@
             },
         });
         $(".pxl-tabs-carousel").on("beforeChange", function (){
-            alert('hjkadhfkjjdfk');
+            $(".content").find('.pxl-animate').each(function(){
+                var data = $(this).data('settings');
+                $(this).removeClass('animated '+data['animation']).addClass('pxl-invisible');
+            });
         });
     };
     // Make sure you run this code under Elementor.
