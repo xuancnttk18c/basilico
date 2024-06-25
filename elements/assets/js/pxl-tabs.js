33bc66549337449d8dc5f13ac58bc1060cae3d41
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-06-25 11:46:41 +0700

    Update pxl-tabs.js

--- elements/assets/js/pxl-tabs.js
+++ elements/assets/js/pxl-tabs.js
@@ -1,5 +1,6 @@
 ( function( $ ) {
     function basilico_tabs_handler($scope){
+        var link_to_tabs_carousel_id = $scope.find('.link-to-tabs-carousel-id').text().trim();
         $scope.find(".pxl-tabs .tabs-title .tab-title").on("click", function(e){
             e.preventDefault();
             var target = $(this).data("target");
@@ -17,7 +18,16 @@
                 }, data['animation_delay']);
 
             });
+            if ($scope.find('.pxl-tabs-carousel').length > 0 && link_to_tabs_carousel_id.length == 0)
+                $scope.find('.pxl-tabs-carousel').slick('refresh');
         });
+
+        if (link_to_tabs_carousel_id != undefined) {
+            $scope.find('[data-slide]').click(function(e){
+                var slideno = $(this).data('slide');
+                $('#' + link_to_tabs_carousel_id).slick('slickGoTo', slideno);
+            });
+        }
     }
 
     // Make sure you run this code under Elementor.
