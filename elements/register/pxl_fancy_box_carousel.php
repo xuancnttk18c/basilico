201daf07fd51937621666cbf9e63578000ba10b3
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-05 10:20:47 +0700

    Update pxl_fancy_box_carousel.php

--- elements/register/pxl_fancy_box_carousel.php
+++ elements/register/pxl_fancy_box_carousel.php
@@ -261,7 +261,6 @@ pxl_add_custom_widget(
                             'type' => \Elementor\Controls_Manager::COLOR,
                             'selectors' => [
                                 '{{WRAPPER}} .pxl-fancy-box-carousel .btn:hover' => 'border-color: {{VALUE}};',
-                                '{{WRAPPER}} .pxl-fancy-box-carousel .btn.btn-additional-6' => 'background-color: {{VALUE}};',
                                 '{{WRAPPER}} .pxl-fancy-box-carousel .btn:not(.btn-additional-7):before' => 'background-color: {{VALUE}};',
                                 '{{WRAPPER}} .pxl-fancy-box-carousel .btn.btn-additional-7 .pxl-button-bg' => 'background-color: {{VALUE}};',
                             ],
