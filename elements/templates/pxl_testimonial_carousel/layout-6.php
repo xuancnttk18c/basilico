22502b42b4abf23796ddcc061f4a317775a18d3e
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-08 09:00:25 +0700

    update

--- elements/templates/pxl_testimonial_carousel/layout-6.php
+++ elements/templates/pxl_testimonial_carousel/layout-6.php
@@ -18,7 +18,7 @@ if(!empty($settings['button_link']['url'])){
     }
 }
 $link_attributes = $widget->get_render_attribute_string( 'link' );
-   
+
 $settings = array_merge($default_settings, $settings);
 extract($settings);
 
@@ -31,27 +31,27 @@ $pagination_style = basilico()->get_theme_opt('swiper_pagination_style', 'style-
 
 $opts = [
     'slide_direction'               => 'horizontal',
-    'slide_percolumn'               => '1',
+    'slide_percolumn'               => 1,
     'slide_mode'                    => 'slide',
-    'slides_to_show_xxl'            => $widget->get_setting('col_xxl', '4'), 
-    'slides_to_show'                => $widget->get_setting('col_xl', '4'), 
-    'slides_to_show_lg'             => $widget->get_setting('col_lg', '3'), 
-    'slides_to_show_md'             => $widget->get_setting('col_md', '3'), 
-    'slides_to_show_sm'             => $widget->get_setting('col_sm', '2'), 
-    'slides_to_show_xs'             => $widget->get_setting('col_xs', '1'), 
-    'slides_to_scroll'              => $widget->get_setting('slides_to_scroll', '1'), 
-    'slides_gutter'                 => 30, 
+    'slides_to_show_xxl'            => $widget->get_setting('col_xxl', 4), 
+    'slides_to_show'                => $widget->get_setting('col_xl', 4), 
+    'slides_to_show_lg'             => $widget->get_setting('col_lg', 3), 
+    'slides_to_show_md'             => $widget->get_setting('col_md', 3), 
+    'slides_to_show_sm'             => $widget->get_setting('col_sm', 2), 
+    'slides_to_show_xs'             => $widget->get_setting('col_xs', 1), 
+    'slides_to_scroll'              => $widget->get_setting('slides_to_scroll', 1), 
+    'slides_gutter'                 => 30,
     'arrow'                         => $arrows,
     'dots'                          => $dots,
     'dots_style'                    => 'bullets',
-    'autoplay'                      => $widget->get_setting('autoplay', 'false'),
-    'pause_on_hover'                => $widget->get_setting('pause_on_hover', 'true'),
-    'pause_on_interaction'          => 'true',
-    'delay'                         => $widget->get_setting('autoplay_speed', '5000'),
-    'loop'                          => $widget->get_setting('infinite','true'),
-    'speed'                         => $widget->get_setting('speed', '500'),
+    'autoplay'                      => $widget->get_setting('autoplay', false),
+    'pause_on_hover'                => $widget->get_setting('pause_on_hover', true),
+    'pause_on_interaction'          => true,
+    'delay'                         => $widget->get_setting('autoplay_speed', 5000),
+    'loop'                          => $widget->get_setting('infinite', true),
+    'speed'                         => $widget->get_setting('speed', 500)
 ];
-  
+
 
 $widget->add_render_attribute( 'carousel', [
     'class'         => 'pxl-swiper-container',
@@ -60,18 +60,18 @@ $widget->add_render_attribute( 'carousel', [
 ]);
 ?>
 <?php if(isset($content_list) && !empty($content_list) && count($content_list)): ?>
-    <div class="pxl-swiper-slider pxl-testimonial-carousel layout-<?php echo esc_attr($settings['layout'])?>">
-        <?php if ($quote_icon_type == 'icon' && !empty($settings['selected_icon']['value'])) { ?>
-            <div class="icon-wrapper">
-                <?php \Elementor\Icons_Manager::render_icon($settings['selected_icon'], ['aria-hidden' => 'true', 'class' => 'item-quote-icon pxl-icon'], 'i'); ?>
-            </div>
-        <?php } ?>
-        <?php if ($quote_icon_type == 'text') { ?>
-            <div class="item-quote-icon">“</div>
-        <?php } ?>
-        <div class="pxl-swiper-slider-wrap pxl-carousel-inner relative">
-            <div <?php pxl_print_html($widget->get_render_attribute_string( 'carousel' )); ?>>
-                <div class="pxl-swiper-wrapper swiper-wrapper">
+<div class="pxl-swiper-slider pxl-testimonial-carousel layout-<?php echo esc_attr($settings['layout'])?>">
+    <?php if ($quote_icon_type == 'icon' && !empty($settings['selected_icon']['value'])) { ?>
+        <div class="icon-wrapper">
+            <?php \Elementor\Icons_Manager::render_icon($settings['selected_icon'], ['aria-hidden' => 'true', 'class' => 'item-quote-icon pxl-icon'], 'i'); ?>
+        </div>
+    <?php } ?>
+    <?php if ($quote_icon_type == 'text') { ?>
+        <div class="item-quote-icon">“</div>
+    <?php } ?>
+    <div class="pxl-swiper-slider-wrap pxl-carousel-inner relative">
+        <div <?php pxl_print_html($widget->get_render_attribute_string( 'carousel' )); ?>>
+            <div class="pxl-swiper-wrapper swiper-wrapper">
                 <?php foreach ($content_list as $key => $value):
                     $description = isset($value['description']) ? $value['description'] : '';
                     $title       = isset($value['title']) ? $value['title'] : '';
@@ -100,77 +100,77 @@ $widget->add_render_attribute( 'carousel', [
                         </div>
                     </div>
                 <?php endforeach; ?>
-                </div>
             </div>
-            <?php if($arrows !== 'false'): ?>
-                <div class="pxl-swiper-arrows style-default nav-vertical-out">
-                    <div class="pxl-swiper-arrow pxl-swiper-arrow-prev"><span class="pxli-arrow-left"></span></div>
-                    <div class="pxl-swiper-arrow pxl-swiper-arrow-next"><span class="pxli-arrow-right"></span></div>
-                </div>
-            <?php endif; ?>
-            <?php if($dots !== 'false'): ?>
-                <div class="pxl-swiper-dots <?php echo esc_attr($pagination_style); ?>"></div>
-            <?php endif; ?>
         </div>
-        <?php
-        $thumb_opts = [
-            'allow_touch_move'              => 'false',
-            'slide_direction'               => 'horizontal',
-            'slide_percolumn'               => '1',
-            'slide_mode'                    => 'slide', 
-            'slides_to_show_xxl'            => '5',
-            'slides_to_show'                => '5', 
-            'slides_to_show_lg'             => '4', 
-            'slides_to_show_md'             => '3', 
-            'slides_to_show_sm'             => '2', 
-            'slides_to_show_xs'             => '1', 
-            'slides_to_scroll'              => '1', 
-            'slides_gutter'                 => 20,
-            'arrow'                         => 'false',
-            'dots'                          => 'false',
-            'speed'                         => '500',
-        ];
-        $data_thumb_settings = wp_json_encode($thumb_opts);
-        ?>
-        <div class="pxl-swiper-slider-thumbs d-flex">
-            <div class="pxl-swiper-slider-inner pxl-carousel-inner">
-                <div class="pxl-swiper-thumbs overflow-hidden" data-settings="<?php echo esc_attr($data_thumb_settings) ?>">
-                    <div class="pxl-thumbs-wrapper swiper-wrapper ">
-                        <?php
-                        $idx = 0;
-                        foreach ($content_list as $key => $value):
-                            $idx++;
-                            $image       = isset($value['image']) ? $value['image'] : [];
-                            $thumbnail = '';
-                            if(!empty($image['id'])) {
-                                $img = pxl_get_image_by_size( array(
-                                    'attach_id'  => $image['id'],
-                                    'thumb_size' => '250x250',
-                                    'class' => 'no-lazyload',
-                                ));
-                                $thumbnail = $img['thumbnail'];
-                            }
-                            ?>
-                            <div class="pxl-swiper-slide thumb-item swiper-slide">
-                                <div class="thumbs-wrap">
-                                    <div class="item-wrap">
-                                        <?php if(!empty($thumbnail)) :?>
-                                            <div class="item-image col-auto">
-                                                <span class="img-outer">
-                                                    <?php echo wp_kses_post($thumbnail); ?>
-                                                </span>
-                                            </div>
-                                        <?php endif; ?>
-                                    </div>
+        <?php if($arrows !== 'false'): ?>
+            <div class="pxl-swiper-arrows style-default nav-vertical-out">
+                <div class="pxl-swiper-arrow pxl-swiper-arrow-prev"><span class="pxli-arrow-left"></span></div>
+                <div class="pxl-swiper-arrow pxl-swiper-arrow-next"><span class="pxli-arrow-right"></span></div>
+            </div>
+        <?php endif; ?>
+        <?php if($dots !== 'false'): ?>
+            <div class="pxl-swiper-dots <?php echo esc_attr($pagination_style); ?>"></div>
+        <?php endif; ?>
+    </div>
+    <?php
+    $thumb_opts = [
+        'allow_touch_move'              => 'false',
+        'slide_direction'               => 'horizontal',
+        'slide_percolumn'               => '1',
+        'slide_mode'                    => 'slide', 
+        'slides_to_show_xxl'            => '5',
+        'slides_to_show'                => '5', 
+        'slides_to_show_lg'             => '4', 
+        'slides_to_show_md'             => '3', 
+        'slides_to_show_sm'             => '2', 
+        'slides_to_show_xs'             => '1', 
+        'slides_to_scroll'              => '1', 
+        'slides_gutter'                 => 20,
+        'arrow'                         => 'false',
+        'dots'                          => 'false',
+        'speed'                         => '500',
+    ];
+    $data_thumb_settings = wp_json_encode($thumb_opts);
+    ?>
+    <div class="pxl-swiper-slider-thumbs d-flex">
+        <div class="pxl-swiper-slider-inner pxl-carousel-inner">
+            <div class="pxl-swiper-thumbs overflow-hidden" data-settings="<?php echo esc_attr($data_thumb_settings) ?>">
+                <div class="pxl-thumbs-wrapper swiper-wrapper ">
+                    <?php
+                    $idx = 0;
+                    foreach ($content_list as $key => $value):
+                        $idx++;
+                        $image       = isset($value['image']) ? $value['image'] : [];
+                        $thumbnail = '';
+                        if(!empty($image['id'])) {
+                            $img = pxl_get_image_by_size( array(
+                                'attach_id'  => $image['id'],
+                                'thumb_size' => '250x250',
+                                'class' => 'no-lazyload',
+                            ));
+                            $thumbnail = $img['thumbnail'];
+                        }
+                        ?>
+                        <div class="pxl-swiper-slide thumb-item swiper-slide">
+                            <div class="thumbs-wrap">
+                                <div class="item-wrap">
+                                    <?php if(!empty($thumbnail)) :?>
+                                        <div class="item-image col-auto">
+                                            <span class="img-outer">
+                                                <?php echo wp_kses_post($thumbnail); ?>
+                                            </span>
+                                        </div>
+                                    <?php endif; ?>
                                 </div>
                             </div>
-                        <?php endforeach; ?>
-                    </div>
+                        </div>
+                    <?php endforeach; ?>
+                    <?php if ($show_button == 'true') : ?>
+                        <a class="btn-circle-more" <?php echo implode( ' ', [ $link_attributes ] ) ?>>...</a>
+                    <?php endif; ?>
                 </div>
             </div>
-            <?php if ($show_button == 'true') : ?>
-                <a class="btn-circle-more" <?php echo implode( ' ', [ $link_attributes ] ) ?>>...</a>
-            <?php endif; ?>
         </div>
     </div>
+</div>
 <?php endif; ?>
