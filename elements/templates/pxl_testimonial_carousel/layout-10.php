67841310479e381864cc97ed806e8df442c544f7
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-08 09:36:08 +0700

    Update layout-10.php

--- elements/templates/pxl_testimonial_carousel/layout-10.php
+++ elements/templates/pxl_testimonial_carousel/layout-10.php
@@ -103,8 +103,8 @@ $widget->add_render_attribute( 'carousel', [
         </div>
         <?php if($arrows !== 'false'): ?>
             <div class="pxl-swiper-arrows style-default nav-vertical-out">
-                <div class="pxl-swiper-arrow pxl-swiper-arrow-prev"><span class="pxli-arrow-left"></span></div>
-                <div class="pxl-swiper-arrow pxl-swiper-arrow-next"><span class="pxli-arrow-right"></span></div>
+                <div class="pxl-swiper-arrow pxl-swiper-arrow-prev"><span class="zmdi zmdi-arrow-left"></span></div>
+                <div class="pxl-swiper-arrow pxl-swiper-arrow-next"><span class="zmdi zmdi-arrow-right"></span></div>
             </div>
         <?php endif; ?>
         <?php if($dots !== 'false'): ?>
