7d4838e13843193032eb01a03687f2540f2ab335
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-09 14:05:37 +0700

    Update layout-1.php

--- elements/templates/pxl_heading/layout-1.php
+++ elements/templates/pxl_heading/layout-1.php
@@ -10,13 +10,6 @@ $default_settings = [
     'text_list' => '',
 ];
 $settings = array_merge($default_settings, $settings);
-$hightlight_list = $widget->get_settings('text_list');
-$list_array = [];
-if(count($hightlight_list) > 0){
-    foreach ($hightlight_list as $key => $list) {
-        $list_array[] = $list['highlight_text'];
-    }
-}
 $widget->add_render_attribute( 'wrap-heading', 'class', 'pxl-heading-wrap d-flex layout1');
 
 $widget->add_render_attribute( 'large-title', 'class', 'heading-title');
@@ -100,18 +93,9 @@ extract($settings);
         <?php endif; ?>
 
         <<?php echo esc_attr($title_tag); ?> <?php pxl_print_html($widget->get_render_attribute_string( 'large-title' )); ?>>
-            <<?php echo implode( ' ', [ $inner_tag, $link_attributes ] ); ?>>
-                <?php pxl_print_html(nl2br($title)); ?>
-            </<?php pxl_print_html($inner_tag); ?>>
-            <?php
-            if(!empty($list_array)){
-                ?>
-                <span class="heading-highlight typewrite" data-period="3500" data-type="<?php echo esc_attr(json_encode($list_array)); ?>">
-                    <span class="wrap"></span>
-                </span>
-                <?php
-            }
-            ?>
+        <<?php echo implode( ' ', [ $inner_tag, $link_attributes ] ); ?>>
+        <?php pxl_print_html(nl2br($title)); ?>
+        </<?php pxl_print_html($inner_tag); ?>>
         </<?php echo esc_attr($title_tag); ?>>
         <?php if(!empty($description)) : ?>
             <div <?php pxl_print_html($widget->get_render_attribute_string( 'description' )); ?>>
