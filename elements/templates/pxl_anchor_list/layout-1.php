9e993cfe6bb3c4158eb71b0547b662d23c0d69b8
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-09 15:41:29 +0700

    Update layout-1.php

--- elements/templates/pxl_anchor_list/layout-1.php
+++ elements/templates/pxl_anchor_list/layout-1.php
@@ -9,21 +9,22 @@ extract($settings);
 
 <?php if (isset($anchors) && !empty($anchors) && count($anchors)): ?>
 <div class="pxl-anchor-list layout-1">
-	<?php foreach ($anchors as $key => $anchor): ?>
-		<?php
-		$template = (int)$anchor['template'];
-		$widget->add_render_attribute('anchor'.$key, 'class', 'pxl-anchor side-panel');
-		$target = '.pxl-hidden-template-'.$template;
+	<div class="pxl-anchor-list-wrap d-inline-flex">
+		<?php foreach ($anchors as $key => $anchor): ?>
+			<?php
+			$template = (int)$anchor['template'];
+			$widget->add_render_attribute('anchor'.$key, 'class', 'pxl-anchor side-panel');
+			$target = '.pxl-hidden-template-'.$template;
 
-		if ($template > 0 ){
-			if ( !has_action( 'pxl_anchor_target_hidden_panel_'.$template) ){
-				add_action( 'pxl_anchor_target_hidden_panel_'.$template, 'basilico_hook_anchor_hidden_panel' );
+			if ($template > 0 ){
+				if ( !has_action( 'pxl_anchor_target_hidden_panel_'.$template) ){
+					add_action( 'pxl_anchor_target_hidden_panel_'.$template, 'basilico_hook_anchor_hidden_panel' );
+				}
+			} else {
+				add_action( 'pxltheme_anchor_target', 'basilico_hook_anchor_custom' );
 			}
-		} else {
-			add_action( 'pxltheme_anchor_target', 'basilico_hook_anchor_custom' );
-		}
-		?>
-		<div class="pxl-anchor-list-wrap d-inline-flex">
+			?>
+			
 			<a href="#pxl-<?php echo esc_attr($template)?>" <?php pxl_print_html($widget->get_render_attribute_string( 'anchor'.$key )); ?> data-target="<?php echo esc_attr($target)?>">
 				<?php
 				echo '<div class="pxl-anchor-icon d-inline-flex align-items-center justify-content-center">';
@@ -31,7 +32,7 @@ extract($settings);
 				echo '</div>';
 				?>
 			</a>
-		</div>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
+	</div>
 </div>
 <?php endif; ?>
