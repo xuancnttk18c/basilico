03adf1ab83ca36323c74e647bc31865c5a8cc9ea
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-11 11:23:50 +0700

    Update pxl-tabs.js

--- elements/assets/js/pxl-tabs.js
+++ elements/assets/js/pxl-tabs.js
@@ -3,13 +3,6 @@
     function basilico_tabs_handler($scope){
         var link_to_tabs_carousel_id = $scope.find('.link-to-tabs-carousel-id').text().trim();
 
-        $scope.find(".pxl-tabs .tabs-title .tab-title").on("click", function(e){
-            e.preventDefault();
-            $(this).addClass('active').siblings().removeClass('active');
-            var target = $(this).data("target");
-            basilico_tab_active(target);
-        });
-
         function basilico_tab_active(target) {
             $(target).addClass('active').siblings().removeClass('active'); 
             $(target).siblings().find('.pxl-animate').each(function(){
@@ -27,12 +20,12 @@
                 $scope.find('.pxl-tabs-carousel').slick('refresh');
         }
 
-        if (link_to_tabs_carousel_id != undefined) {
-            $scope.find('[data-slide]').click(function(e){
-                var slideno = $(this).data('slide');
-                $('#' + link_to_tabs_carousel_id).slick('slickGoTo', slideno);
-            });
-        }
+        $scope.find(".pxl-tabs .tabs-title .tab-title").on("click", function(e){
+            e.preventDefault();
+            $(this).addClass('active').siblings().removeClass('active');
+            var target = $(this).data("target");
+            basilico_tab_active(target);
+        });
 
         if ($scope.find(".pxl-tabs .pxl-tabs-arrows")) {
             $scope.find(".pxl-tabs .pxl-tabs-arrows .pxl-tab-arrow-next").on("click", function(e) {
@@ -43,18 +36,7 @@
                     $scope.find(".pxl-tabs .tabs-title .tab-title.active").first().addClass('active').siblings().removeClass('active');
 
                 var target = $scope.find(".pxl-tabs .tabs-title .tab-title.active").data("target");
-                $(target).addClass('active').siblings().removeClass('active'); 
-                $(target).siblings().find('.pxl-animate').each(function(){
-                    var data = $(this).data('settings');
-                    $(this).removeClass('animated '+data['animation']).addClass('pxl-invisible');
-                });
-                $(target).find('.pxl-animate').each(function(){
-                    var data = $(this).data('settings');
-                    var cur_anm = $(this);
-                    setTimeout(function () {  
-                        $(cur_anm).removeClass('pxl-invisible').addClass('animated ' + data['animation']);
-                    }, data['animation_delay']);
-                });
+                basilico_tab_active(target);
             });
             $scope.find(".pxl-tabs .pxl-tabs-arrows .pxl-tab-arrow-prev").on("click", function(e) {
                 e.preventDefault();
@@ -64,18 +46,14 @@
                     $scope.find(".pxl-tabs .tabs-title .tab-title.active").last().addClass('active').siblings().removeClass('active');
 
                 var target = $scope.find(".pxl-tabs .tabs-title .tab-title.active").data("target");
-                $(target).addClass('active').siblings().removeClass('active'); 
-                $(target).siblings().find('.pxl-animate').each(function(){
-                    var data = $(this).data('settings');
-                    $(this).removeClass('animated '+data['animation']).addClass('pxl-invisible');
-                });
-                $(target).find('.pxl-animate').each(function(){
-                    var data = $(this).data('settings');
-                    var cur_anm = $(this);
-                    setTimeout(function () {  
-                        $(cur_anm).removeClass('pxl-invisible').addClass('animated ' + data['animation']);
-                    }, data['animation_delay']);
-                });
+                basilico_tab_active(target);
+            });
+        }
+
+        if (link_to_tabs_carousel_id != undefined) {
+            $scope.find('[data-slide]').click(function(e){
+                var slideno = $(this).data('slide');
+                $('#' + link_to_tabs_carousel_id).slick('slickGoTo', slideno);
             });
         }
     }
