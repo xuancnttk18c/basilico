c0889b6cb29e0d05c1a80aaf7438ea93aa68362d
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-11 11:33:06 +0700

    Update pxl-tabs.js

--- elements/assets/js/pxl-tabs.js
+++ elements/assets/js/pxl-tabs.js
@@ -41,6 +41,7 @@
 
                 var target = $scope.find(".pxl-tabs .tabs-title .tab-title.active").data("target");
                 basilico_tab_active(target);
+
                 if (link_to_tabs_carousel_id != undefined) {
                     var slideno = $scope.find(".pxl-tabs .tabs-title .tab-title.active").data('slide');
                     $('#' + link_to_tabs_carousel_id).slick('slickGoTo', slideno);
@@ -55,6 +56,11 @@
 
                 var target = $scope.find(".pxl-tabs .tabs-title .tab-title.active").data("target");
                 basilico_tab_active(target);
+
+                if (link_to_tabs_carousel_id != undefined) {
+                    var slideno = $scope.find(".pxl-tabs .tabs-title .tab-title.active").data('slide');
+                    $('#' + link_to_tabs_carousel_id).slick('slickGoTo', slideno);
+                }
             });
         }
 
