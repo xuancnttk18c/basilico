fa1fdb419748032919e3cab91c4422978a82fcf7
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-11 15:50:48 +0700

    update

--- elements/templates/pxl_menu_list/layout-6.php
+++ elements/templates/pxl_menu_list/layout-6.php
@@ -42,7 +42,7 @@ $item_animation_delay = !empty($item_animation_delay) ? $item_animation_delay : 
                 $data_settings = 'data-settings="' . esc_attr($data_animation) . '"';
             }
             ?>
-            <div class="pxl-menu-item<?php echo esc_attr($value['is_featured'] == 'yes' ? ' featured' : ''); ?> <?php echo esc_attr($animate_cls); ?>" <?php pxl_print_html($data_settings); ?>>
+            <div class="pxl-menu-item<?php echo esc_attr($value['is_featured'] == 'yes' ? ' featured' : ''); ?> <?php echo esc_attr($animate_cls); ?> <?php echo 'elementor-repeater-item-' . $value['_id'] ?>" <?php pxl_print_html($data_settings); ?>>
                 <?php if ($value['tag_1'] === 'yes' || $value['tag_2'] === 'yes') : ?>
                     <div class="custom-tags d-flex">
                         <?php if ($value['tag_1'] === 'yes' && !empty($value['tag_1_text'])) : ?>
