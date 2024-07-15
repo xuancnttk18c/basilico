590c8c00c6726bb8dde04b2b3dc3ad281857a5b4
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-15 09:10:53 +0700

    Update layout-1.php

--- elements/templates/pxl_anchor_list/layout-1.php
+++ elements/templates/pxl_anchor_list/layout-1.php
@@ -57,8 +57,7 @@ extract($settings);
 					\Elementor\Icons_Manager::render_icon( $anchor['selected_icon'], [ 'aria-hidden' => 'true', 'class' => '' ], 'span' );
 					?>
 				</a>
-				<?php var_dump($anchor['cart_count']); ?>
-				<?php if ( !\Elementor\Plugin::$instance->editor->is_edit_mode() && esc_attr($anchor['cart_count'] == 'true') ): ?>
+				<?php if ( !\Elementor\Plugin::$instance->editor->is_edit_mode() && isset($anchor['cart_count']) && esc_attr($anchor['cart_count'] == 'true') ): ?>
 					<span class="anchor-cart-count"><?php echo WC()->cart->cart_contents_count; ?></span>
 				<?php endif; ?>
 				<?php if ($selected_template == 'cart-dropdown' && !\Elementor\Plugin::$instance->editor->is_edit_mode()): ?>
