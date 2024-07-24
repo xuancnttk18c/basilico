1d95b4e84ee96a34d67c3231fe2f376a6a983b02
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-07-24 14:07:32 +0700

    Update layout-12.php

--- elements/templates/pxl_testimonial_carousel/layout-12.php
+++ elements/templates/pxl_testimonial_carousel/layout-12.php
@@ -72,8 +72,8 @@ $widget->add_render_attribute('carousel', [
                     ?>
                     <div class="pxl-swiper-slide swiper-slide">
                         <div class="item-inner row">
-                            <div class="item-image col-4" style="background-image: url(<?php echo esc_attr($image['id']) ? esc_url($image['url']) : ''; ?>); "></div>
-                            <div class="item-content col-8">
+                            <div class="item-image col-5" style="background-image: url(<?php echo esc_attr($image['id']) ? esc_url($image['url']) : ''; ?>); "></div>
+                            <div class="item-content col-7">
                                 <?php if (!empty($testimonial_title)) { ?>
                                     <h4 class="testimonial-title"><span><?php echo esc_html($testimonial_title); ?></span></h4>
                                 <?php } ?>
