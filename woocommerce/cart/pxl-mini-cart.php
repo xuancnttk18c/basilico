02e8ff80b977c22c3d6451957c66fcb014d97d1e
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-01 15:13:58 +0700

    update

--- woocommerce/cart/pxl-mini-cart.php
+++ woocommerce/cart/pxl-mini-cart.php
@@ -104,6 +104,17 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 		do_action( 'woocommerce_mini_cart_contents' );
 		?>
 	</ul>
+	<div class="cart-footer-inner">
+		<div class="woocommerce-mini-cart__total total">
+			<span class="total-lbl"><?php echo esc_html__( 'Subtotal: ', 'basilico' ); ?></span>
+			<span class="total-value"><?php echo WC()->cart->get_cart_subtotal(); ?></span>
+		</div>
+		<?php do_action( 'woocommerce_widget_shopping_cart_before_buttons' ); ?>
+		<div class="woocommerce-mini-cart__buttons buttons">
+			<?php do_action( 'woocommerce_widget_shopping_cart_buttons' ); ?>	
+		</div>
+		<?php do_action( 'woocommerce_widget_shopping_cart_after_buttons' ); ?>
+	</div>
 <?php else : ?>
 	<?php wc_get_template( 'cart/cart-empty.php' ); ?>
 <?php endif; ?>
