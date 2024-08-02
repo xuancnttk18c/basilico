93bf0e2e0a6d30d0d86b78d68f37e3cb5baa207c
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-02 10:24:08 +0700

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -726,8 +726,10 @@
                 success: function( response ) {  
                     $( document.body ).trigger( 'wc_fragment_refresh' );
                     $( document.body ).trigger( 'basilico_update_qty', [ item_key, item_qty ] );
-                    console.log(response);
                 },
+                complete: function(response) {
+                    console.log(response);
+                }
             } );
         });
         $('.pxl-sticky-atc').on( 'change', '.qty', function() {
