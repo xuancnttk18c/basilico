5abe21fddb27d7c0d9aae06796d2edbfbb9154bd
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-02 15:12:47 +0700

    update

--- woocommerce/cart/pxl-cart-empty.php
+++ woocommerce/cart/pxl-cart-empty.php
@@ -3,7 +3,7 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<div class="pxl-cart-empty">
+<div class="pxl-cart-empty d-flex align-items-center">
 	<div class="empty-icon">
 		<i class="pxli pxli-cart-plus"></i>
 	</div>
