f8a29d38f1d1f9b92a22045f46e6fad766c6863c
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-08 17:13:40 +0700

    Update _hook_single.php

--- woocommerce/_hook_single.php
+++ woocommerce/_hook_single.php
@@ -517,30 +517,28 @@ function basilico_simple_add_to_cart(){
     if ( $product->is_in_stock() ) : ?>
     	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
     	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
-    		<div class="d-flex align-items-center">
-	    		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
-	    		<?php
-	    		do_action( 'woocommerce_before_add_to_cart_quantity' );
-	    		woocommerce_quantity_input(
-	    			array(
-	    				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-	    				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-	                    'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( sanitize_text_field( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-	                )
-	    		);
-	    		do_action( 'woocommerce_after_add_to_cart_quantity' );
-	    		?>
-	    		<div class="pxl-addtocart-btn-wrap">
-	    			<div class="pxl-atc-btn">
-	    				<?php
-	    				$add_to_cart_btn_style = basilico()->get_theme_opt('add_to_cart_button_style', 'btn-outline-secondary');
-	    				$single_btn_cls = 'pxl-btn single_add_to_cart_button button alt '.esc_attr($add_to_cart_btn_style);
-	    				?>
-	    				<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="<?php echo esc_attr($single_btn_cls); ?>"><span><?php echo esc_html( $product->single_add_to_cart_text() ); ?></span></button>
-	    			</div>
-	    		</div>
-	    		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+    		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+    		<?php
+    		do_action( 'woocommerce_before_add_to_cart_quantity' );
+    		woocommerce_quantity_input(
+    			array(
+    				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+    				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+                    'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( sanitize_text_field( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+                )
+    		);
+    		do_action( 'woocommerce_after_add_to_cart_quantity' );
+    		?>
+    		<div class="pxl-addtocart-btn-wrap">
+    			<div class="pxl-atc-btn">
+    				<?php
+    				$add_to_cart_btn_style = basilico()->get_theme_opt('add_to_cart_button_style', 'btn-outline-secondary');
+    				$single_btn_cls = 'pxl-btn single_add_to_cart_button button alt '.esc_attr($add_to_cart_btn_style);
+    				?>
+    				<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="<?php echo esc_attr($single_btn_cls); ?>"><span><?php echo esc_html( $product->single_add_to_cart_text() ); ?></span></button>
+    			</div>
     		</div>
+    		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
     	</form>
     	<?php
     	$product_wishlist = basilico()->get_theme_opt('product_wishlist', '0');
