a522e1f6a3356b190cbea8fb71d25a6d52fe09b6
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-15 08:48:11 +0700

    update

--- woocommerce/_hook.php
+++ woocommerce/_hook.php
@@ -53,6 +53,9 @@ function basilico_wc_update_get_template($template, $template_name, $args, $temp
         case 'global/quantity-input.php':
             $template = get_template_directory().'/'.WC()->template_path().'global/pxl-quantity-input.php';
             break;
+        case 'cart/cart.php':
+            $template = get_template_directory().'/'.WC()->template_path().'cart/pxl-cart.php';
+            break;
     }
 
     return $template;
