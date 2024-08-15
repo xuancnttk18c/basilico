01c2cc65863e121dcd8b71be5930e99477890c53
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-15 10:04:52 +0700

    Update wc-function.php

--- woocommerce/wc-function.php
+++ woocommerce/wc-function.php
@@ -120,3 +120,25 @@ function basilico_get_grid_gutter_y_class($option_name, $is_single = false){
     }
     return $g_y_cls;
 }
+
+function basilico_wc_cart_totals_shipping_method_label( $method ) {
+    $label     = $method->get_label();
+    $has_cost  = 0 < $method->cost;
+    $hide_cost = ! $has_cost && in_array( $method->get_method_id(), array( 'free_shipping', 'local_pickup' ), true );
+
+    if ( $has_cost && ! $hide_cost ) {
+        if ( WC()->cart->display_prices_including_tax() ) {
+            $label .= ' (' . wc_price( $method->cost + $method->get_shipping_tax() ).')';
+            if ( $method->get_shipping_tax() > 0 && ! wc_prices_include_tax() ) {
+                $label .= ' <small class="tax_label">' . WC()->countries->inc_tax_or_vat() . '</small>';
+            }
+        } else {
+            $label .= ' (' . wc_price( $method->cost ).')';
+            if ( $method->get_shipping_tax() > 0 && wc_prices_include_tax() ) {
+                $label .= ' <small class="tax_label">' . WC()->countries->ex_tax_or_vat() . '</small>';
+            }
+        }
+    }
+
+    return apply_filters( 'woocommerce_cart_shipping_method_full_label', $label, $method );
+}
