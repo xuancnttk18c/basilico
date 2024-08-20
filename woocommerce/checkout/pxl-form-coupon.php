cbde5b2dc76cebc6aecfb6eb3c83e982da66e948
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-20 09:01:37 +0700

    update

--- woocommerce/checkout/pxl-form-coupon.php
+++ woocommerce/checkout/pxl-form-coupon.php
@@ -12,7 +12,6 @@ if ( ! wc_coupons_enabled() ) {
  
 	<div class="form-coupon-inner relative"> 
 		<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Enter promo code', 'basilico' ); ?>" id="coupon_code" value="" />
-		<span class="pxl-icon lnil lnil-offer"></span>
 		<button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply', 'basilico' ); ?>"><?php esc_html_e( 'Apply', 'basilico' ); ?></button>
 	</div>
  
