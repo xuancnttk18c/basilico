fec5cedecace9aaad9674657602094f1225c4a8b
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-20 10:17:47 +0700

    Update _hook_checkout.php

--- woocommerce/_hook_checkout.php
+++ woocommerce/_hook_checkout.php
@@ -42,7 +42,7 @@ if(!function_exists('basilico_woocommerce_order_button_html')){
 	add_filter('woocommerce_order_button_html', 'basilico_woocommerce_order_button_html');
 	function basilico_woocommerce_order_button_html(){
 		$checkout_btn_style = basilico()->get_theme_opt('checkout_button_style', 'btn-outline');
-		$order_button_text = apply_filters( 'woocommerce_order_button_text', esc_html__( '<span class="pxl-button-text">Place order</span>', 'basilico' ) );
-		return '<div class="pxl-checkout-place-order"><button type="submit" class="button ' . esc_attr($checkout_btn_style) . '" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button></div>';
+		$order_button_text = apply_filters( 'woocommerce_order_button_text', esc_html__( 'Place order', 'basilico' ) );
+		return '<div class="pxl-checkout-place-order"><button type="submit" class="button ' . esc_attr($checkout_btn_style) . '" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">'.'<span class="pxl-button-text">' . esc_html( $order_button_text ) . '</span>'.'</button></div>';
 	}
 }
