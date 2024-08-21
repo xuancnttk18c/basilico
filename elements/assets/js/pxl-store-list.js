0ab9a90621fdd5f8e8965bdd739a9d90295a6805
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-21 17:27:59 +0700

    Update pxl-store-list.js

--- elements/assets/js/pxl-store-list.js
+++ elements/assets/js/pxl-store-list.js
@@ -1,5 +1,6 @@
 ( function( $ ) {
     var PXLStoreListHander = function( $scope, $ ) {
+        alert('kfhjksdfhdshfjkdhfj')
         $scope.find('.pxl-store').on('click', function() {
             $(this).addClass('active').siblings().removeClass('active');
             $scope.find('store-submit').attr('href', $(this).data('url'));
@@ -7,6 +8,6 @@
     };
     // Make sure you run this code under Elementor.
     $( window ).on( 'elementor/frontend/init', function() {
-        elementorFrontend.hooks.addAction( 'frontend/element_ready/pxl_store_list.default', PXLStoreListHander );
+        elementorFrontend.hooks.addAction( 'frontend/element_ready/pxl_store_select.default', PXLStoreListHander );
     } );
 } )( jQuery );
