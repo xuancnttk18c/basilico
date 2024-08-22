820864a8985b67fe8d68d1d3ccb61c1eadea00b4
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-08-22 09:20:42 +0700

    Update pxl-store-list.js

--- elements/assets/js/pxl-store-list.js
+++ elements/assets/js/pxl-store-list.js
@@ -4,7 +4,7 @@
             $(this).addClass('selected').siblings().removeClass('selected');
             console.log($(this).data('url'));
             $scope.find('.btn.store-submit').attr('href', $(this).data('url'));
-            $scope.find('.btn.store-submit').css('cursor', 'pointer');
+            $scope.find('.btn.store-submit').css('cursor', 'wait');
         })
     };
     // Make sure you run this code under Elementor.
