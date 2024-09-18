a79e5ead12df47f75eca64d00eed47dca6224d95
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-09-18 14:31:02 +0700

    Update layout-1.php

--- elements/templates/pxl_contact_form/layout-1.php
+++ elements/templates/pxl_contact_form/layout-1.php
@@ -6,7 +6,6 @@ $settings = array_merge($default_settings, $settings);
 extract($settings);
 $html_id = pxl_get_element_id($settings);
 $ctf7_id = '';
-$select_style = esc_attr($select_style) == '' ? $select_style : '';
 
 if(class_exists('WPCF7') && !empty($ctf7_slug)){
     $cf7 = get_posts('post_type="wpcf7_contact_form"&numberposts=-1');
