980306338bd32fd893246a53cc8b1ab3f56386cb
Thomas <153076498+Thomas7iquid@users.noreply.github.com>
2024-09-19 17:30:54 +0700

    Update layout-5.php

--- elements/templates/pxl_testimonial_carousel/layout-5.php
+++ elements/templates/pxl_testimonial_carousel/layout-5.php
@@ -6,9 +6,7 @@ $default_settings = [
 $settings = array_merge($default_settings, $settings);
 extract($settings);
 
-$arrows = $widget->get_setting('arrows', 'false');
 $arrows_style = $widget->get_setting('arrows_style', 'style-1');
-$dots = $widget->get_setting('dots', 'false');
 $quote_icon_type = $widget->get_setting('quote_icon_type', 'text');
 
 $pagination_style = basilico()->get_theme_opt('swiper_pagination_style', 'style-df');
@@ -106,29 +104,8 @@ $widget->add_render_attribute('carousel', [
                 <?php endforeach; ?>
             </div>
         </div>
-        <?php if ($arrows !== 'false') : ?>
-            <div class="pxl-swiper-arrows nav-vertical-out <?php echo esc_attr($arrows_style);?>">
-                <div class="pxl-swiper-arrow pxl-swiper-arrow-next">
-                    <?php 
-                    if ( $settings['arrow_icon_next']['value'] ) 
-                        \Elementor\Icons_Manager::render_icon( $settings['arrow_icon_next'], [ 'aria-hidden' => 'true', 'class' => 'pxl-icon'], 'span' );
-                    else
-                        echo '<span class="pxl-icon pxli-arrow-next"></span>';
-                    ?>
-                </div>
-                <div class="pxl-swiper-arrow pxl-swiper-arrow-prev">
-                    <?php 
-                    if ( $settings['arrow_icon_previous']['value'] ) 
-                        \Elementor\Icons_Manager::render_icon( $settings['arrow_icon_previous'], [ 'aria-hidden' => 'true', 'class' => 'pxl-icon'], 'span' );
-                    else
-                        echo '<span class="pxl-icon pxli-arrow-prev"></span>';
-                    ?>
-                </div>
-            </div>
-        <?php endif; ?>
-        <?php if ($dots !== 'false') : ?>
-            <div class="pxl-swiper-dots <?php echo esc_attr($pagination_style); ?>"></div>
-        <?php endif; ?>
+        <?php basilico_arrow_template($settings); ?>
+        <div class="pxl-swiper-dots <?php echo esc_attr($pagination_style); ?>"></div>
     </div>
 </div>
 <?php endif; ?>
